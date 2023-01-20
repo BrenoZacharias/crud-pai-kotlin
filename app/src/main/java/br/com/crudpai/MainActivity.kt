b97699cee3cf26b538366a71db1d12110f7d7ab5
BrenoZacharias <brenomzacharias@gmail.com>
2023-01-20 20:19:30 -0300

    Adição e,onfigurações e Customizaçãoo Toolbar

--- app/src/main/java/br/com/crudpai/MainActivity.kt
+++ app/src/main/java/br/com/crudpai/MainActivity.kt
@@ -2,10 +2,20 @@ package br.com.crudpai
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        NavigationUI.setupWithNavController(findViewById(R.id.app_toolbar), navController, appBarConfiguration)
     }
 }
