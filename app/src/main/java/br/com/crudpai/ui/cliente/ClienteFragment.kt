5560ae0c4dffe61ba50bf61d02428838b1ad7f4e
BrenoZacharias <brenomzacharias@gmail.com>
2023-01-23 20:17:26 -0300

    Atualização do build.gradle, começo da inligação entre ClienteFragment e ClienteViewModel

--- app/src/main/java/br/com/crudpai/ui/cliente/ClienteFragment.kt
+++ app/src/main/java/br/com/crudpai/ui/cliente/ClienteFragment.kt
@@ -1,21 +1,71 @@
 package br.com.crudpai.ui.cliente
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import br.com.crudpai.data.db.AppDatabase
+import br.com.crudpai.data.db.dao.ClienteDAO
+import br.com.crudpai.repository.ClienteRepository
+import br.com.crudpai.repository.DatabaseDataSourceCliente
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.Snackbar
 import br.com.crudpai.R
+import br.com.crudpai.extensions.hideKeyboard
+import kotlinx.android.synthetic.main.fragment_cliente.*
 
-class ClienteFragment : Fragment() {
+class ClienteFragment : Fragment(R.layout.fragment_cliente) {
 
-    private lateinit var viewModel: ClienteViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_cliente, container, false)
+    private val viewModel: ClienteViewModel by viewModels {
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                val clienteDAO : ClienteDAO =
+                    AppDatabase.getInstance(requireContext()).clienteDAO
+                val repository : ClienteRepository = DatabaseDataSourceCliente(clienteDAO)
+                return ClienteViewModel(repository) as T
+            }
+        }
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeEvents()
+        setListeners()
+    }
+
+    private fun observeEvents() {
+        viewModel.subscriberStateEventData.observe(viewLifecycleOwner) { clienteState ->
+            when (clienteState) {
+                is ClienteViewModel.ClienteState.Inserted -> {
+                    clearFields()
+                    hideKeyboard()
+                }
+            }
+        }
+        viewModel.messageEventData.observe(viewLifecycleOwner) { stringResId ->
+            Snackbar.make(requireView(), stringResId, Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    private fun clearFields() {
+        input_razao_social.text?.clear()
+        input_email.text?.clear()
+    }
+
+    private fun hideKeyboard() {
+        val parentActivity = requireActivity()
+        if (parentActivity is AppCompatActivity) {
+            parentActivity.hideKeyboard()
+        }
+    }
+
+    private fun setListeners() {
+        button_subscriber.setOnClickListener {
+            val razaoSocial = input_razao_social.text.toString()
+            val email = input_email.text.toString()
+        }
+    }
 }
--- app/src/main/java/br/com/crudpai/ui/cliente/ClienteViewModel.kt
+++ app/src/main/java/br/com/crudpai/ui/cliente/ClienteViewModel.kt
@@ -1,7 +1,51 @@
 package br.com.crudpai.ui.cliente
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.com.crudpai.R
+import br.com.crudpai.data.db.entity.TelefoneEntity
+import br.com.crudpai.repository.ClienteRepository
+import kotlinx.coroutines.launch
 
-class ClienteViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ClienteViewModel(
+    private val repository: ClienteRepository
+) : ViewModel() {
+
+    private val _clienteStateEventData = MutableLiveData<ClienteState>()
+    val subscriberStateEventData: LiveData<ClienteState>
+        get() = _clienteStateEventData
+
+    private val _messageEventData = MutableLiveData<Int>()
+    val messageEventData: LiveData<Int>
+        get() = _messageEventData
+
+    fun addCliente(razaoSocial : String, nomeFantasia : String, cnpj : String,
+                   fones : List<TelefoneEntity>, email: String, municipioEndereco : String,
+                   logradouroEndereco : String, numeroEndereco : String,
+                   descricaoEndereco : String) = viewModelScope.launch {
+        try {
+            val id = repository.insertCliente(razaoSocial, nomeFantasia, cnpj,
+                fones, email, municipioEndereco,
+            logradouroEndereco, numeroEndereco,
+            descricaoEndereco)
+            if (id > 0) {
+                _clienteStateEventData.value = ClienteState.Inserted
+                _messageEventData.value = R.string.cliente_inserted_successfully
+            }
+        } catch (ex : Exception){
+            _messageEventData.value = R.string.cliente_error_to_insert
+            Log.e(TAG, ex.toString())
+        }
+    }
+
+    sealed class ClienteState {
+        object Inserted : ClienteState()
+    }
+
+    companion object {
+        private val TAG = ClienteViewModel::class.java.simpleName
+    }
 }
